c051a5c49ee71de1c827abcee0516f25042c8fe5
DongjuLee <dozoo0528@gmail.com>
2024-11-05 16:23:19 +0900

    파일 추가

---
+++ dack/src/test/java/com/example/dack/DackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
