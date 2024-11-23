6dedd8a1e5ea26ee4ccb36727ce45332c5a142ea
DongjuLee <dozoo0528@gmail.com>
2024-11-24 01:45:22 +0900

    작ㅇㅇㅇ

--- src/main/java/com/example/back/cloud/service/FileStorageService.java
+++ src/main/java/com/example/back/cloud/service/FileStorageService.java
@@ -9,7 +9,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Service
-public class FileStorageService {
+public class
+FileStorageService {
 
     private final String storageDirectory = "./uploaded_files";
 
