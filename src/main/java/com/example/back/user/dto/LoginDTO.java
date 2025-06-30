e057e4d786784c0b9a4fc908ae11a776620bfb62
DongjuLee <dozoo0528@gmail.com>
2025-06-30 20:48:06 +0900

    주석

--- src/main/java/com/example/back/user/dto/LoginDTO.java
+++ src/main/java/com/example/back/user/dto/LoginDTO.java
@@ -14,7 +14,7 @@ public class LoginDTO {
 
     @NotNull(message = "이메일은 필수 입력 값입니다.")
     @Email(message = "유효한 이메일 형식이어야 합니다.")
-    private String userEmail; // 이메일
+    private String userEmail; // 이메일ㅇ
 
     @NotNull(message = "비밀번호는 필수 입력 값입니다.")
     @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
